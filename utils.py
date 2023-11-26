72c542660d705b50b5f8d0ea261b116dad1c4b04
Doan Pham <doannphamm57@gmail.com>
2023-11-26 14:19:49 +0700

    feat: add utils method

---
+++ utils.py
@@ -0,0 +1,23 @@
+import os
+import torch
+
+
+def save_checkpoint(state, dataset, filename="checkpoint.pth.tar"):
+    """Saves checkpoint to disk"""
+    directory = "runs/%s/" % (dataset)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    filename = directory + filename
+    torch.save(state, filename)
+
+
+def get_dimensions_and_lengths(lst):
+    dimensions = 0
+    lengths = []
+
+    while isinstance(lst, list):
+        dimensions += 1
+        lengths.append(len(lst))
+        lst = lst[0] if len(lst) > 0 else None  
+        
+        return dimensions, lengths
