070ea8697be6f978761cb46a4bd6578673926b84
Doan Pham <doannphamm57@gmail.com>
2023-11-26 14:19:35 +0700

    misc: add logs

--- text_encoder.py
+++ text_encoder.py
@@ -17,13 +17,18 @@ class TextEncoder(nn.Module):
         self.fc_layer = nn.Linear(768, 64)  # Assuming SentenceBERT output size is 768
 
     def forward(self, x):
-        if not isinstance(x, list):
-            x = [x]
-
         # Tokenize the text data
+        print("-" * 10)
+        # print(f"TextEncoder - initial x: {x}")
         tokens = self.tokenizer(
             x, return_tensors="pt", padding=True, truncation=True, max_length=512
         )
         embeddings = self.sentence_bert(**tokens).last_hidden_state.mean(dim=1)
+
+        # print(f"TextEncoder - embeddings.shape: {embeddings.shape}")
         x = self.fc_layer(embeddings)
+
+        print(f"TextEncoder - x's shape after fc_layer: {x.shape}")
+        print("-" * 10)
+
         return x
