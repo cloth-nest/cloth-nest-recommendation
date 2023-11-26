ac0a53c4484b415d335a12c391b52501ecddf6ab
Doan Pham <doannphamm57@gmail.com>
2023-11-26 14:20:01 +0700

    misc: add logs

--- image_encoder.py
+++ image_encoder.py
@@ -1,3 +1,5 @@
+from regex import F
+import torch
 import torch.nn as nn
 import math
 import torch.utils.model_zoo as model_zoo
@@ -6,14 +8,27 @@ from torchvision.models import resnet18
 
 # Define the Image Encoder using ResNet18
 class ImageEncoder(nn.Module):
-    def __init__(self):
+    def __init__(self, embedding_dim=64):
         super(ImageEncoder, self).__init__()
         self.resnet18 = resnet18(pretrained=True)
-        self.embedding_layer = nn.Linear(
-            1000, 64
+        self.fc_layer = nn.Linear(
+            1000, embedding_dim
         )  # Assuming ResNet18 output size is 1000
 
     def forward(self, x):
+        print("-" * 10)
+        # print(f"ImageEncoder - initial x's shape: {x.shape}")
+        # print(f"ImageEncoder - initial x: {x}")
+        # Ensure x has at least 3 dimensions
+        if len(x.shape) < 3:
+            x = x.unsqueeze(0)
+
         x = self.resnet18(x)
-        x = self.embedding_layer(x)
+        # print(f"ImageEncoder - after resnet18 x's shape: {x.shape}")
+        # print(f"ImageEncoder - after resnet18 x: {x}")
+
+        x = self.fc_layer(x)
+        print(f"ImageEncoder - after fc_layer x's shape: {x.shape}")
+        print("-" * 10)
+
         return x
