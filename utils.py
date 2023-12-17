d4cbfcd82570ef1573ec1e223cf7d6e934c9f4d8
Doan Pham <doannphamm57@gmail.com>
2023-12-17 13:32:32 +0700

    fix: product existence check if faulty

--- utils.py
+++ utils.py
@@ -1,14 +1,21 @@
+import logging
+
+
 def check_if_product_exist(product_id, products_info_catalog):
     """
     Check if a product already exists in the Recommendation System's catalog by comparing id
     """
 
+    logging.debug(
+        f"utils.py - check_if_product_exist(); product_id: {product_id}, products_info_catalog: {products_info_catalog}"
+    )
+
     # Check if the list is not empty
     if products_info_catalog:
         for product_dictionary in products_info_catalog:
             if "id" in product_dictionary and product_dictionary["id"] == product_id:
-                return False
+                return True
 
-        return True
+        return False
     else:
         return False
