c1799113b3db1634cde64f39fc5f45947c73c60f
hungmai2411 <maiphamquochung@gmail.com>
2023-12-24 17:54:59 +0700

    add outfit dataset

--- outfit_dataset.py
+++ outfit_dataset.py
@@ -1,3 +1,4 @@
+import json
 import logging
 import os
 from torch.utils.data import Dataset
@@ -18,6 +19,7 @@ class OutfitDataset(Dataset):
     ):
         """
         Create a Dataset of outfits for compatibility prediction.
+
         Parameters:
         - data_directory (str): The path to data directory which should contain the "polyvore_outfits" directory
         - polyvore_split (str): The split of the polyvore dataset, could be disjoint or non-disjoint
@@ -25,6 +27,7 @@ class OutfitDataset(Dataset):
         - transform: The transformation for items' images
         """
         try:
+            # region [1] Loading file paths
             item_metadata_json_path = os.path.join(
                 data_directory,
                 POLYVORE_OUTFITS_DIR_NAME,
@@ -35,8 +38,31 @@ class OutfitDataset(Dataset):
                 data_directory, POLYVORE_OUTFITS_DIR_NAME, polyvore_split
             )
 
-            data_json_path = os.path.join(
+            data_json_file_path = os.path.join(
                 polyvore_split_root_dir, f"{split}.json")
+
+            compatibility_file_path = os.path.join(
+                polyvore_split_root_dir, f"compatibility_{split}.txt"
+            )
+
+            logging.debug(
+                f"outfit_dataset.py - __init__() - [1] Loading file paths: \n - item_metadata_json_path: {item_metadata_json_path}; \n - data_json_file_path: {data_json_file_path}; \n - compatibility_file_path: {compatibility_file_path}"
+            )
+            # endregion
+
+            try:
+                with open(data_json_file_path, "r") as data_json_file:
+                    outfit_data = json.load(data_json_file)
+
+            except FileNotFoundError:
+                logging.error(
+                    f"outfit_dataset.py - __init__() - [2] File not found: {data_json_file_path}"
+                )
+            except json.JSONDecodeError:
+                logging.error(
+                    f"outfit_dataset.py - __init__() - [2] Error decoding JSON in file: {data_json_file_path}"
+                )
+
         except Exception as e:
             logging.exception(
                 f"outfit_dataset.py - __init__() - exception: {e}")
